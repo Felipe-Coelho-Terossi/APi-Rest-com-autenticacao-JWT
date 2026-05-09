5e9697c210ec8b35d7c3c7918eda34eb1966632d
Felipe-Coelho-Terossi <felipeterossi2@gmail.com>
2026-05-09 15:05:41 -0300

    fim2

--- src/app.ts
+++ src/app.ts
@@ -6,9 +6,15 @@ import authRoutes from './routes/auth.routes';
 dotenv.config();
 
 const app = express();
+
 app.use(cors({
   origin: [
     'http://localhost:5173',
     'https://a-pi-rest-com-autenticacao-jwt.vercel.app'
   ]
 }));
+
+app.use(express.json());
+app.use('/auth', authRoutes);
+
+export default app;
